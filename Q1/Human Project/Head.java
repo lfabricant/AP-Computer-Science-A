048081dc1b72580991f7426ce0f534b8cdf49760
Lindsay Fabricant <68440117+lfabricant@users.noreply.github.com>
2021-10-22 18:23:37 -0400

    Create Head.java

---
+++ Q1/Human Project/Head.java
@@ -0,0 +1,21 @@
+
+public class Head
+{
+   /**
+      Constructor for objects of class head
+     */
+   public void draw()
+   {
+       System.out.println("♫        ♫         ♫");
+       System.out.println("✿________♕________✿");
+       System.out.println("|   |  __   __  |   | ");
+       System.out.println("|   |--|♥|-|♥|--|   | ");
+       System.out.println("|   |           |   | ");
+       System.out.println("|   |     ʖ     |   | ");
+       System.out.println("|   |  (  ‿  )  |   | ");
+       System.out.println("|   |___________|   |   ");
+       System.out.println("|       |   |       | ");
+       
+
+    }
+}
