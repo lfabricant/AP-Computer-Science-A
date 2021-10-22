f041d9365a4f8a1767578aa53e1f7bd27dd46bfe
Lindsay Fabricant <68440117+lfabricant@users.noreply.github.com>
2021-10-22 18:04:45 -0400

    Create HelloWorld.java

---
+++ Q1/Hello World/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ *Displays the text "Hello World" on the terminal window.
+ * 
+ * @author (Lindsay Fabricant) 
+ * @version (9/4/19)
+ */
+public class HelloWorld // classs heading
+{
+   public static void main(String[] args) // method heading
+   {
+       System.out.print("Hello World");
+   }
+}
