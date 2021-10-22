b0e98e16ec398c834574450564cf213f78f9137b
Lindsay Fabricant <68440117+lfabricant@users.noreply.github.com>
2021-10-22 18:24:12 -0400

    Create HumanClient.java

---
+++ Q1/Human Project/HumanClient.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class HumanClient here.
+ *
+ * @author (Lindsay)
+ * @version (a version number or a date)
+ */
+public class HumanClient
+{
+    public static void main(String[] args)
+    {
+        Body b = new Body();
+        Head h = new Head();
+        h.draw();
+        b.draw();
+    }
+}
