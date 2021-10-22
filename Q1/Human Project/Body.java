b887f54094f8e3b74bc6b1f88d5026989bc00b71
Lindsay Fabricant <68440117+lfabricant@users.noreply.github.com>
2021-10-22 18:23:12 -0400

    Create Body.java

---
+++ Q1/Human Project/Body.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Body here.
+ *
+ * @author (Lindsay Fabricant)
+ * @version (9/18/19)
+ */
+public class Body
+{
+    /**
+      Constructor for objects of class Body
+     */
+    public void draw()
+    {
+        System.out.println("       _    _     ");
+        System.out.println("      | | | | |     ");
+        System.out.println("✌    |♫⚛ツ⚡☮☘|     ✊ ");
+        System.out.println("\\ \\___| | | | |___/⌚/ ");
+        System.out.println(" \\_\\__| | | | |__/_/");
+        System.out.println("      |_|_|_|_|   ");
+        System.out.println("      /       \\  ");
+        System.out.println("     /         \\ ");
+        System.out.println("    /___________\\ ");
+        System.out.println("   /__|      |__\\ ");
+        System.out.println("    👠         👠   ");
+        
+    }
+}
