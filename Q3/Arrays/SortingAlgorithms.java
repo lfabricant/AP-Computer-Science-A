0beffe9c467bb78b4d3c5f26882b51e02410d009
Lindsay Fabricant <68440117+lfabricant@users.noreply.github.com>
2021-10-22 23:32:14 -0400

    Create SortingAlgorithms.java

---
+++ Q3/Arrays/SortingAlgorithms.java
@@ -0,0 +1,59 @@
+public class SortingAlgorithms {
+    public static void selectionSort(int[] nums){
+         int index = 0;
+         for(int i = 0; i < nums.length; i++){
+           if(nums[i] < nums[index]){
+                 nums = sort(i,index, nums);
+                 index = i;
+           }
+         }
+         printHelperInt(nums);
+    }
+    public static int[] sort(int from, int to, int[] arr){
+           int temp = arr[from];
+           arr[to] = temp;
+           arr[from] = to;
+           return arr;
+    }
+    public static void printHelperInt(int[] arr){
+        int lastIndex = arr.length-1;
+        System.out.print("{");
+        for(int i = 0; i < arr.length-1; i++){
+            System.out.print(arr[i] + ",");
+        }
+        System.out.print(arr[lastIndex] + "}");
+    }
+    public static void main(String[] args){
+        int arrays = (int)((Math.random()*6)+5);
+        for(int i = 0; i < arrays; i++){
+            int[] arr = randArrayGenerator(1,100,(int)(Math.random()*6+15));
+           System.out.println("Create an array: ");
+            printHelperInt(arr);
+            System.out.println("Sort an array: ");
+            selectionSort(arr);
+            System.out.println();
+            System.out.println();
+            insertionSort(arr);
+        }
+    }
+    private static int[] randArrayGenerator(int start, int end, int numTerms){
+        int[] arr = new int[numTerms];
+        for(int i = 0; i < numTerms; i++){
+            int val = (int)(Math.random()*(end-start+1)+start);
+            arr[i] = val;
+        }
+        return arr;
+    }
+    public static void insertionSort(int[] elements){
+        for(int j = 1; j < elements.length; j++){
+            int temp = elements[j];
+            int p = j;
+            while(p > 0 && temp < elements[p-1]){
+                elements[p] = elements[p-1];
+                p--;
+            }
+            elements[p] = temp;
+        }
+        printHelperInt(elements);
+    }
+}
